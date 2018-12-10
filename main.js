2258fb540b46b246fa62c21f14c17c9387bc14ac
anupamagarg <dolly.singla@gmail.com>
2018-12-10 10:01:03 -0600

    Works as per requirments in READMe

---
+++ main.js
@@ -0,0 +1,48 @@
+const hamburger = {
+	name: 'Hamburger',
+	type: 'beef',
+	cooked: false,
+}
+const zucchini = {
+	name: 'Zucchini',
+	type: 'vegetable',
+	cooked: false,
+}
+const chickenBreast = {
+	name: 'Chicken Breast',
+	type: 'chicken',
+	cooked: false,
+}
+const corn = {
+	name: 'Corn',
+	type: 'vegetable',
+	cooked: false,
+}
+const steak = {
+	name: 'Steak',
+	type: 'beef',
+	cooked: false,
+}
+
+// An first array containing the objects to be cooked.
+const uncookedFood = [hamburger, zucchini, chickenBreast, corn, steak];
+
+// An empty array that will store the objects after the `grill()` function cooks the food.
+const cookedFood = [];
+
+function grill (currentObject) {
+    
+         currentObject.cooked = true;
+         cookedFood.push(currentObject);
+    
+};
+uncookedFood.forEach((item)=>{
+    var currentObject = item;
+    grill(item)
+    
+   })
+console.log(uncookedFood);
+console.log(cookedFood);
+
+
+    
